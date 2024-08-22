70fc1a793b4705df3424b29efa271dcb30de2db6
coalbush03 <143535354+coalbush03@users.noreply.github.com>
2024-08-22 22:49:35 +0200

    import

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 #load all the data from apis
 #use spotipy
+
+import spotipy
+import spotipy.oauth2 as oauth2
+import streamlit as st
+import pandas as pd
+import os
+
+CLIENT_ID = os.environ.get('CLIENT_ID')
